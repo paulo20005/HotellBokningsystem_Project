27cc01f34def7287c347a15354470a85f3e2c2fb
Kristian Tawfeeq <kristiantawfeeq@MacBook-Pro-som-tillhor-Kristian.local>
2026-03-12 11:09:14 +0100

    Lagt till CORS så frontend funkar

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import sqlite3
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 @app.get("/")
 def root():
